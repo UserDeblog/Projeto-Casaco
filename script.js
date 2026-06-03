9057b5fdbe48dfb4b8d6faee669fe4f015133440
UserDeblog <samuelwilliam.official@gmail.com>
2026-06-03 17:54:53 -0300

    change photo

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function toggleMode() {
   const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
   if (html.classList.contains("light")) {
     html.classList.remove("light")
+    img.setAttribute("src", "./assets/avatar.png")
   } else {
     html.classList.add("light")
+    img.setAttribute("src", "./assets/avatar-light.png")
   }
+}
+
+const img = document.querySelector("#profile img")
+const html = document.documentElement
+
+if (html.classList.contains("light")) {
+  img.setAttribute("src", "./assets/avatar-light.png")
+} else {
+  img.setAttribute("src", "./assets/avatar.png")
 }
